c6b5db26c8cc8a59d1429b538794a09b72828628
Kevin R. Stravers <kevinrs@stud.ntnu.no>
2013-11-02 15:40:31 +0100

    Debugging Macros (TTL_DEBUG_*)

--- include/Debug/Debug.hpp
+++ include/Debug/Debug.hpp
@@ -9,23 +9,29 @@
 
 
 #ifdef NDEBUG
-    #define dthrow(x) ((void)0)
-    #define dif(x) ((void)0);
+    #define TTL_DEBUG_THROW(x) ((void)0)
+    #define TTL_DEBUG_IF(x) ((void)0);
+    #define TTL_DEBUG_ELSE ((void)0);
+    #define TTL_DEBUG_EXE(x) ((void)0)
 #else
-    #define dthrow(x)   \
-        throw std::runtime_error \
-        (   \
-            std::string("Exception")    \
-            + "\nMessage: "    \
-            + x \
-            + "\nFile: "   \
-            + __FILE__ \
-            + "\nFunction: "    \
-            + __func__  \
-            + "\nLine: "    \
-            + std::to_string(__LINE__)  \
+    #define TTL_DEBUG_THROW(x)                          \
+        throw std::runtime_error                        \
+        (                                               \
+            std::string("START OF EXCEPTION MESSAGE")   \
+            + "\nMessage: "                             \
+            + x                                         \
+            + "\nFile: "                                \
+            + __FILE__                                  \
+            + "\nFunction: "                            \
+            + __func__                                  \
+            + "\nLine: "                                \
+            + std::to_string(__LINE__)                  \
+            + std::string("\nEND OF EXCEPTION MESSAGE")    \
         )
-    #define dif(x) if(x)
+    #define TTL_DEBUG_IF(x) if(x)
+    #define TTL_DEBUG_ELSE else
+    #define TTL_DEBUG_EXE(x) x()
 #endif // NDEBUG
+
 
 #endif // DEBUG_HPP_INCLUDED
