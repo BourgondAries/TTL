409acb1cd9a0d2858033aaadf302ff782a278f4b
Kevin R. Stravers <kevinrs@stud.ntnu.no>
2013-12-15 12:34:19 +0100

    Added the new definition

---
+++ include/Ttldef/Ttldef.hpp
@@ -0,0 +1,15 @@
+#ifndef TTLDEF_HPP_INCLUDED
+#define TTLDEF_HPP_INCLUDED
+
+// Headers
+#include <cstddef>
+
+
+namespace ttl
+{
+
+    typedef std::size_t sti;
+
+} // Namespace ttl
+
+#endif // TTLDEF_HPP_INCLUDED
